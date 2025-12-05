acc7f485afc0697e5bbe9b244341abf558368af1
keita kato <infotec62509@gmail.com>
2025-12-06 00:57:08 +0900

    商品説明の追加、戻る、変更を保存、削除ボタンの追加

--- src/resources/views/livewire/detail.blade.php
+++ src/resources/views/livewire/detail.blade.php
@@ -1,25 +1,39 @@
-<form action="/products/{{ $product['id'] }}/update" class="detail-page__content" method="post">
-    @csrf
-    <input type="hidden" name="id" value="{{ $product['id'] }}">
-    <div class="content__product">
-        <div class="product-image">
-            @if ($product_image)
+<div class="detail-page__content">
+
+    <form action="/products/{{ $product['id'] }}/update" class="product-form" method="post">
+        @csrf
+        <input type="hidden" name="id" value="{{ $product['id'] }}">
+        <div class="product-form__content">
+            <div class="product-image">
+                @if ($product_image)
                 <img src="{{ $product_image->temporaryUrl() }}" alt="" class="product-image__img">
-            @else
+                @else
                 <img src="{{ asset($product['image']) }}" alt="" class="product-image__img">
-            @endif
-            <input type="file" wire:model="product_image" accept="image/png,image/jpeg" class="product-image__input">
+                @endif
+                <input type="file" wire:model="product_image" accept="image/png,image/jpeg" class="product-image__input">
+            </div>
+            <div class="product-items">
+                <label class="product-item__header">商品名</label>
+                <input type="text" class="product-item__input-text" name="name" value="{{ $product['name'] }}">
+                <label class="product-item__header">値段</label>
+                <input type="text" class="product-item__input-text" name="price" value="{{ $product['price'] }}">
+                <label class="product-item__header">季節</label>
+                @foreach ($seasons as $season)
+                <input type="checkbox" name="season_id[]" id="season{{ $season['id'] }}" class="product-item__input-checkbox" @foreach ($product['seasons'] as $product_season) @if ($season['id'] === $product_season['id']) checked @endif @endforeach>
+                <label for="season{{ $season['id'] }}" class="product-item__checkbox-label">{{ $season['name'] }}</label>
+                @endforeach
+            </div>
+            <label class="product-item__header-description">商品説明</label>
+            <textarea name="description" class="product-description">{{ $product['description'] }}</textarea>
         </div>
-        <div class="product-items">
-            <label class="product-item__header">商品名</label>
-            <input type="text" class="product-item__input" name="name" value="{{ $product['name'] }}">
-            <label class="product-item__header">値段</label>
-            <input type="text" class="product-item__input" name="price" value="{{ $product['price'] }}">
-            <label class="product-item__header">季節</label>
-            @foreach ($seasons as $season)
-                <input type="checkbox" name="season{{ $season['id'] }}" id="season{{ $season['id'] }}" class="product-item__input-season" @foreach ($product['seasons'] as $product_season) @if ($season['id'] === $product_season['id']) checked @endif @endforeach>
-                <label for="season{{ $season['id'] }}" class="product-item__season-label">{{ $season['name'] }}</label>
-            @endforeach
+        <div class="product-form__buttons">
+            <a href="/products" class="product-form__button-back">戻る</a>
+            <button type="submit" class="product-form__button-submit">変更を保存</button>
         </div>
-    </div>
-</form>
+    </form>
+    <form action="/products/{{ $product['id'] }}/delete" method="post" class="delete_form">
+        @method('DELETE')
+        @csrf
+        <button class="delete_form__button">削除</button>
+    </form>
+</div>
